32204ac1d11c8a9d8c03b40d6173eafb0ac715b3
gabtab <byrne.gavin5@gmail.com>
2017-08-05 19:16:00 +0100

    Add files via upload

--- Run through file and create tables using excel.py
+++ Run through file and create tables using excel.py
@@ -1,34 +1,43 @@
-
 import time
 import openpyxl
 import pandas as pd
 
+#parse excel file into dataframe used for looping through sensor names data
 wb = pd.ExcelFile('D:/College/Proposal 2/Crash data/Test Table with VCG for File Creation.xlsx')
-
 df = wb.parse("Sheet1")
-
+#select the folder where all of the unzipped files are saved
 pathextract = 'D:/College/Proposal 2/Crash data/All data zipped/Full files/'
 pathinput = 'D:/College/Proposal 2/Crash data/Sensor Data'
-
+#manipulate the new data frame so the name of the file names in the data frame match match the name of the sensor data files
 df['testtext'] = df['TSTNO'].astype(str)
 df['testtext'] = df['testtext'].apply(lambda x: x.zfill(5))
 df['testtext'] = 'v' + df['testtext'].astype(str)
-
 df['curvetext'] = df[' CURNO'].astype(str)
 df['curvetext'] = df['curvetext'].apply(lambda x: x.zfill(3))
 df['curvetext'] = '.' + df['curvetext'].astype(str)
-
 df['search'] = df['testtext']+df['curvetext']
 del df['testtext']
 del df['curvetext']
 
-searchfile = open(pathextract +'v00006.001', "r")
+#loop through the files and create a dataframe with the columns time and force, the curno and the tstno
 
-preparation['preparation'] = searchfile
+##for test ######CHANGE THE DATA FRAME BACK TO DF1 AT THE END #######################################################
 
-        search the pathextract folder for this file
-        create an excel file in pathinput called preparation with all the data in it tstno and curvo as columns
-        convert to flat file
-        combine with the sensordata file
-        if its equal to 
-        linknumber = str(cell.value)
+df1 = df.iloc[0:3,:]
+############################################################################################
+for i, row in enumerate(df1.itertuples(),1):
+    
+    newdata = pd.read_csv(pathextract +row.search, sep = "\t", header=None)
+    newdata.columns = ["Time", "Force"]
+    newdata['TSTNO'] = df1.iloc[i-1, 0]
+    newdata[' CURNO'] = df1.iloc[i-1, 2]
+    #need to then combine the data with the previous data in one dataframe
+    if i == 1:   
+        masterdata = pd.DataFrame()
+        masterdata = newdata
+    else:
+        masterdata = masterdata.append(newdata)
+        
+    writer = pd.ExcelWriter('output.xlsx')
+    masterdata.to_excel(writer,'Sheet1')
+    writer.save()
