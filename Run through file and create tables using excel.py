1a3424f70311fdff5e8692ba1af601f10f26e8e3
gabtab <byrne.gavin5@gmail.com>
2017-08-05 19:42:48 +0100

    Add files via upload

--- Run through file and create tables using excel.py
+++ Run through file and create tables using excel.py
@@ -7,7 +7,7 @@ wb = pd.ExcelFile('D:/College/Proposal 2/Crash data/Test Table with VCG for File
 df = wb.parse("Sheet1")
 #select the folder where all of the unzipped files are saved
 pathextract = 'D:/College/Proposal 2/Crash data/All data zipped/Full files/'
-pathinput = 'D:/College/Proposal 2/Crash data/Sensor Data'
+pathinput = 'D:/College/Proposal 2/Crash data/Sensor Data/masterfile.csv'
 #manipulate the new data frame so the name of the file names in the data frame match match the name of the sensor data files
 df['testtext'] = df['TSTNO'].astype(str)
 df['testtext'] = df['testtext'].apply(lambda x: x.zfill(5))
@@ -21,16 +21,13 @@ del df['curvetext']
 
 #loop through the files and create a dataframe with the columns time and force, the curno and the tstno
 
-##for test ######CHANGE THE DATA FRAME BACK TO DF1 AT THE END #######################################################
-
-df1 = df.iloc[0:3,:]
 ############################################################################################
-for i, row in enumerate(df1.itertuples(),1):
+for i, row in enumerate(df.itertuples(),1):
     
     newdata = pd.read_csv(pathextract +row.search, sep = "\t", header=None)
     newdata.columns = ["Time", "Force"]
-    newdata['TSTNO'] = df1.iloc[i-1, 0]
-    newdata[' CURNO'] = df1.iloc[i-1, 2]
+    newdata['TSTNO'] = df.iloc[i-1, 0]
+    newdata[' CURNO'] = df.iloc[i-1, 2]
     #need to then combine the data with the previous data in one dataframe
     if i == 1:   
         masterdata = pd.DataFrame()
@@ -40,4 +37,4 @@ for i, row in enumerate(df1.itertuples(),1):
         
     writer = pd.ExcelWriter('output.xlsx')
     masterdata.to_excel(writer,'Sheet1')
-    writer.save()
+    masterdata.to_csv(pathinput )
