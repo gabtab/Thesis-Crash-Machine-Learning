8c6b1ccd0576ebe6235acedec05b5ac1c7f00044
gabtab <byrne.gavin5@gmail.com>
2017-10-01 20:13:58 +0100

    Add files via upload

--- Data Re-shape and cleanse.R
+++ Data Re-shape and cleanse.R
@@ -1,9 +1,22 @@
+getwd()
+
 library('RODBC')
-?RODBC
+
 dbhandle <- odbcDriverConnect('driver={SQL Server};server=LAPTOP-PAPDF3KG\\SQLEXPRESS;database=CrashData;trusted_connection=true')
 test <- sqlQuery(dbhandle, 'select * from dbo.Sensor_Ouput')
 colnames(test) = c("Signal", "Time","Force", "TSTNO","CURNO","SENATT","AXIS","AXISD")
 
-test = test[test$TSTNO == 6,]
+#test = test[test$TSTNO == 6,]
+#write.table(test, "test1.txt", sep="\t")
+test1 = test[test$TSTNO == 6,]
+resh <- reshape(data = test1, timevar = "AXIS",
+                idvar = c("TSTNO","Time"),
+                drop = c("Signal","AXISD","CURNO", "SENATT"), 
+                direction = "wide")
+?reshape
 
-test
+
+test1 = test[test$TSTNO == 6:100,]
+
+memory.limit(test)
+resh = NULL
